f931b20a9b838eef4c142c61f9569384d47d19a6
ramawama <93228765+ramawama@users.noreply.github.com>
2024-02-19 11:21:24 -0500

    Added hello world for homepage of server. testing stuff

--- server/server.py
+++ server/server.py
@@ -10,5 +10,11 @@ def return_home():
         'Trips': "Mtb biking!"
     })
 
+@app.route("/", methods=['GET'])
+def return_hello():
+    return jsonify({
+        'Msg': "Hello World!"
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)

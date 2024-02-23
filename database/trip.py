4851dc0b7defe295d650263473b247a9830c5ad2
xcao2005 <xcao2005@gmail.com>
2024-02-22 23:46:23 -0500

    trip.py functions including add, delete, update trip, and get trip by id

--- database/trip.py
+++ database/trip.py
@@ -38,20 +38,109 @@ c=conn.cursor()
 #         total_guides_needed INTEGER
 #     )
 # """)
-
 # Insert a record into the trip table; test
 # c.execute("INSERT INTO trip VALUES (1, 'Overnight Adventure', '2021-09-01', '2021-09-03', 2, 4)")
-# conn.commit()
 
-# Query the database and return all records
-c.execute("SELECT * FROM trip")
+def create_trip(id, category, start_date, end_date, lead_guides_needed, total_guides_needed):
+    conn=sqlite3.connect('./trip.db')
+    c=conn.cursor()
+    if not isinstance(id, int):
+        return ("Error: id must be an integer")
+    if not isinstance(category, str):
+        return ("Error: category must be a string") 
+    if not is_trip_type(category):
+        return ("Error: category must be one of the following: 'Overnight Adventure', 'Day Adventure', 'Weeknight Adventure', 'OLC Training TRiP', 'Pro Dev', 'Incentive TRiP', 'Extended TRiP'")
+    if not isinstance(start_date, str):
+        return ("Error: start_date must be a string")
+    if not isinstance(end_date, str):
+        return ("Error: end_date must be a string")
+    if not isinstance(lead_guides_needed, int):
+        return ("Error: lead_guides_needed must be an integer")
+    if not isinstance(total_guides_needed, int):
+        return ("Error: total_guides_needed must be an integer")
+    c.execute("SELECT * FROM trip WHERE id=?", (id,))
+    if c.fetchone() is not None:
+        return ("Error: id must be unique")
+    
+    #everything is checked, except if category is valid within the options
+    try:
+        c.execute("INSERT INTO trip VALUES (?, ?, ?, ?, ?, ?)", (id, category, start_date, end_date, lead_guides_needed, total_guides_needed))
+    except sqlite3.IntegrityError as e:
+        return ("Error: ", e)
+    
+    conn.commit()
+    conn.close()
+    return "Success!"
 
-# Fetch all the records
-records = c.fetchall()
+#questionable: how should we return an error message?
+def get_trip_by_id(id):
+    conn=sqlite3.connect('./trip.db')
+    c=conn.cursor()
+    if not isinstance(id, int):
+        return ("Error: id must be an integer")
+    try:
+        c.execute("SELECT * FROM trip WHERE id=?", (id,))
+    except sqlite3.IntegrityError as e:
+        return ("Error: ", e)
+    result = c.fetchone()
+    conn.close()
+    return result
 
-# Print each record
-for record in records:
-    print(record)
 
-#safely close file
-conn.close()
+def update_trip(id, category, start_date, end_date, lead_guides_needed, total_guides_needed):
+    conn=sqlite3.connect('./trip.db')
+    c=conn.cursor()
+    if not isinstance(id, int):
+        return ("Error: id must be an integer")
+    
+    c.execute("SELECT * FROM trip WHERE id=?", (id,))
+    if c.fetchone() is None:
+        return ("Trip with id {} does not exist".format(id))
+    
+    if not isinstance(category, str):
+        return ("Error: category must be a string") 
+    if not is_trip_type(category):
+        return ("Error: category must be one of the following: 'Overnight Adventure', 'Day Adventure', 'Weeknight Adventure', 'OLC Training TRiP', 'Pro Dev', 'Incentive TRiP', 'Extended TRiP'")
+    if not isinstance(start_date, str):
+        return ("Error: start_date must be a string")
+    if not isinstance(end_date, str):
+        return ("Error: end_date must be a string")
+    if not isinstance(lead_guides_needed, int):
+        return ("Error: lead_guides_needed must be an integer")
+    if not isinstance(total_guides_needed, int):
+        return ("Error: total_guides_needed must be an integer")
+    
+    try:
+        c.execute("UPDATE trip SET category=?, start_date=?, end_date=?, lead_guides_needed=?, total_guides_needed=? WHERE id=?", (category, start_date, end_date, lead_guides_needed, total_guides_needed, id))
+    except sqlite3.InterfaceError as e:
+        return ("Error:", e)
+    conn.commit()
+    conn.close()
+    return "Success!"
+
+def get_all_trips():
+    conn=sqlite3.connect('./trip.db')
+    c=conn.cursor()
+    c.execute("SELECT * FROM trip")
+    records = c.fetchall()
+    conn.close()
+    return records
+
+def delete_trip(id):
+    conn=sqlite3.connect('./trip.db')
+    c=conn.cursor()
+    if not isinstance(id, int):
+        return ("Error: id must be an integer")
+    c.execute("SELECT * FROM trip WHERE id=?", (id,))
+    if c.fetchone() is None:
+        return ("Trip with id {} does not exist".format(id))
+    c.execute("DELETE FROM trip WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return "Success!"
+
+
+def is_trip_type(category):
+    if category not in ['Overnight Adventure', 'Day Adventure', 'Weeknight Adventure', 'OLC Training TRiP', 'Pro Dev', 'Incentive TRiP', 'Extended TRiP']:
+        return False
+    return True

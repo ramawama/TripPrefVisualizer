59bc268061d355d809cc6c5136480a9afd77b2af
ramawama <93228765+ramawama@users.noreply.github.com>
2024-03-03 19:06:10 -0500

    Added a dummy json route for proof of concept

--- server/server.py
+++ server/server.py
@@ -16,5 +16,26 @@ def return_hello():
         'Msg': "Hello World!"
     })
 
+@app.route("/test", methods=['GET'])
+def return_test():
+    table_data = [ # This is a list of dictionaries, we will call in the preferences later after they are set
+        {
+            "id": 1,
+            "name": "Aaron",
+            "status": "Lead Guide",
+            "trip": "Mtb biking"
+        },
+        {
+            "id": 2,
+            "name": "Rama",
+            "status": "Lead Guide",
+            "trip": "Spelunking"
+        }
+    ]
+    return jsonify({
+        'Msg': "Test Table Data",
+        'data': table_data
+    })
+
 if __name__ == "__main__":
     app.run(debug=True)

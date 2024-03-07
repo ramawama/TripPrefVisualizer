940fae072addccfe0465d5afd13e7bdf47682dab
Alina Garib <alinagrb@gmail.com>
2024-03-07 17:57:08 -0500

    Plzwork

---
+++ client/app/schedules/test/page_test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+
+const Test = () =>{
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      fetch("/server/server.py/").then(
+        res => res.json()
+        ).then (
+        data => {
+          setData(data);
+          console.log(data);
+        });
+    }, []);
+    return (
+      <div>
+        
+        {data.map((data) => (
+            <div>{data}</div>
+        ))}
+      </div>
+    );
+};
+
+export default Test
